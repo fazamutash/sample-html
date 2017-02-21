5bbd260d0258437e0c255ade3cdf261072c943d9
fazamutash <muhammadfaza8691@yahoo.com>
2017-02-21 16:20:43 +0700

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -4,8 +4,8 @@ import groovy.json.JsonSlurperClassic
 node {
     env.AWS_DEFAULT_REGION = 'ap-southeast-1'
 	
-	def applicationName = 'agungbd' //change me
-	def deploymentGroupName = 'test' // change me
+	def applicationName = 'fazam' //change me
+	def deploymentGroupName = 'testfaza' // change me
 	def s3BucketName = 'deployment-cdc'
 	
 	//Cleanup workspace
